a53764ae7adb73bba4052782dd2888dce9eb1a3c
SKOT <Korawit_pi@cmu.ac.th>
2022-12-27 16:23:35 +0700

    ข้อ 2

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,14 +2,32 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+double Inter(double PrevBalance,double inter)
+{
+	return PrevBalance*(inter/100);
+}
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+double Tol(double PrevBalance,double Inter)
+{
+	return PrevBalance+Inter;
+}
+
+double New(double Total, double Payment)
+{
+	return Total - Payment;
+}
+
+int main(){	
+	double PrevBalance,Interest,Interest1,Total,Payment,NewBalance;
+	int year = 1;
+
+	cout << "Enter initial loan: ";
+	cin >> PrevBalance;
+	cout << "Enter interest rate per year (%): ";
+	cin >> Interest;
+	cout << "Enter amount you can pay per year: ";
+	cin >> Payment;
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +35,28 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+
+while (PrevBalance > 0)
+{
+	Interest1 = Inter(PrevBalance, Interest);
+	Total = Tol(PrevBalance, Interest1);
+	if (Payment > Total) Payment = Total;
+
+	NewBalance = New(Total,Payment);
+
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << PrevBalance;
+	cout << setw(13) << left << Interest1;
+	cout << setw(13) << left << Total;
+	cout << setw(13) << left << Payment;
+	cout << setw(13) << left << NewBalance;
+	cout << "\n";		
+
+
+	year++;	
+	PrevBalance = NewBalance;
+}
 	
 	return 0;
 }
